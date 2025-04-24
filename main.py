7a2102475a4eacf6299bf40f03fa05b18fc98ccf
Mohd Arshad Khan <osndl.mohdak@aicofindia.com>
2025-04-24 17:16:57 +0530

    new commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Allow requests from JS frontend
+
+@app.route('/api/greet', methods=['GET'])
+def greet():
+    return jsonify({"message": "Hello from Flask!"})
+
+@app.route('/api/echo', methods=['POST'])
+def echo():
+    data = request.get_json()
+    return jsonify({"you_sent": data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
